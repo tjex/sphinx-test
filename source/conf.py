7c177d35b80c928834961ee4018fbff6681d9d18
tjex <tjex@tjex.net>
2024-08-09 15:15:04 +0200

    init so far

---
+++ source/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "sphinx-test"
+copyright = "2024, zk-org"
+author = "zk-org"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["myst_parser"]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "insegel"
+html_static_path = ["_static"]
+globaltoc_maxdepth = 1
+
+# The master toctree document.
+master_doc = "index"
+html_theme_options = {"navigation_depth": 1}
---
+++ source/index.rst
@@ -0,0 +1,54 @@
+.. sphinx-test documentation master file, created by
+   sphinx-quickstart on Fri Aug  9 12:21:00 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+.. toctree::
+   :hidden:
+   :maxdepth: 1
+   :caption: Contents:
+
+   automation <automation> 
+   daily journal <daily journal> 
+   editors integration <editors-integration> 
+   external-call
+   external-processing
+   future-proof
+   getting-started
+   neuron
+   note-creation
+   note-filtering
+   note-format
+   note-frontmatter
+   note-id
+   notebook-housekeeping
+   notebook
+   style
+   tags
+   template-creation
+   template-format
+   template
+   tool-editor
+   tool-fzf
+   tool-pager
+   tool-shell
+
+.. toctree::
+   :hidden:
+   :maxdepth: 1
+   :caption: Configuration
+
+   config-alias
+   config-extra
+   config-filter
+   config-group
+   config-lsp
+   config-note
+   config-notebook
+   config
+
+
+Landing page. Foo bar. Welcome.
+
+Let's :doc:`get started <getting-started>`.
+
